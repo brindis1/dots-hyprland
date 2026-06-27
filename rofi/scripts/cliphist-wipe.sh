a5ef1f8b324f6a83ddf8d4d52cf23cff35968d28
brindis1 <70959446+brindis1@users.noreply.github.com>
2026-06-26 23:52:21 -0600

    height adjust

--- rofi/scripts/cliphist-wipe.sh
+++ rofi/scripts/cliphist-wipe.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-choice=$(printf "󰆴 No\n󰆳 Sí, borrar todo" | rofi -dmenu -i -p "Limpiar historial")
+choice=$(
+printf "󰆴 No\n󰆳 Sí, borrar todo" | rofi \
+    -dmenu \
+    -i \
+    -p "Limpiar historial" \
+    -theme ~/.config/rofi/theme.rasi \
+    -theme-str '
+        window {
+            width: 380px;
+            height: 200px;
+        }
+
+        listview {
+            lines: 2;
+        }
+    '
+)
 
 [[ "$choice" != "󰆳 Sí, borrar todo" ]] && exit
 
